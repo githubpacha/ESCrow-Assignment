fa137aca310c22c42b8ae627826a15e1302434f6
githubpacha <78606232+githubpacha@users.noreply.github.com>
2024-10-23 11:32:02 +0530

    Added Kafka in /routes/dashboard.js

--- Assignment 2/routes/dashboard.js
+++ Assignment 2/routes/dashboard.js
@@ -1,41 +1,81 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const stocks = require('../scripts/stocks');
-const users = require('../scripts/users');
+const stocks = require("../scripts/stocks");
+const users = require("../scripts/users");
+const { Kafka } = require("kafkajs");
 
-
-router.get('/home', (req, res) => {
-    if (req.session.isAuthenticated) {
-        const userStocks = req.session.user.stocks || [];
-        const subscribedStocks = [];
-        userStocks.forEach(stock => {
-            const stockDetail = stocks.find(stockie => stockie.code === stock);
-            if (stockDetail) subscribedStocks.push(stockDetail);
-        });
-        res.render('dashboard', {user: req.session.user, stocks: stocks, subscribedStocks: subscribedStocks});
-    } else {
-        res.redirect('/');
-    }
+const kafka = new Kafka({
+  clientId: "client-dashboard",
+  brokers: ["localhost:9092"],
 });
 
-router.post('/subscribe', (req, res) => {
-    if (req.session.isAuthenticated) {
-        const stock = req.body.stock;
-        if (stock) {
-            const user = users.find(user => user.email === req.session.user.email);
-            user.stocks.push(stock);
-            req.session.user = user;
-        }
-        res.redirect('/home');
-    } else {
-        res.redirect('/');
-    }
-});
+const producer = kafka.producer();
+const consumer = kafka.consumer({ groupId: "stock-updates-group" });
 
-router.get('/logout', (req, res) => {
-    req.session.destroy((err) => {
-        res.redirect('/');
+(async () => {
+  try {
+      await producer.connect();
+      console.log('Kafka Producer connected successfully.');
+
+      await consumer.connect();
+      await consumer.subscribe({ topic: 'stock-updates', fromBeginning: true });
+
+      consumer.run({
+          eachMessage: async ({ topic, partition, message }) => {
+              const updatedStock = JSON.parse(message.value.toString());
+              console.log('Received stock update from Kafka:', updatedStock);
+          }
+      });
+  } catch (error) {
+      console.error('Error connecting to Kafka:', error);
+  }
+})();
+
+router.get("/home", (req, res) => {
+  if (req.session.isAuthenticated) {
+    const userStocks = req.session.user.stocks || [];
+    const subscribedStocks = [];
+    userStocks.forEach((stock) => {
+      const stockDetail = stocks.find((stockie) => stockie.code === stock);
+      if (stockDetail) subscribedStocks.push(stockDetail);
     });
+    res.render("dashboard", {
+      user: req.session.user,
+      stocks: stocks,
+      subscribedStocks: subscribedStocks,
+    });
+  } else {
+    res.redirect("/");
+  }
+});
+
+router.post("/subscribe", async (req, res) => {
+  if (req.session.isAuthenticated) {
+    const stock = req.body.stock;
+    if (stock) {
+      const user = users.find((user) => user.email === req.session.user.email);
+      user.stocks.push(stock);
+      req.session.user = user;
+
+      const stockDetail = stocks.find(stockie => stockie.code === stock);
+      await producer.send({
+        topic: 'stock-updates',
+        messages: [
+            {value: JSON.stringify(stockDetail)}
+        ]
+      });
+      console.log('Write:', stockDetail);
+    }
+    res.redirect("/home");
+  } else {
+    res.redirect("/");
+  }
+});
+
+router.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
